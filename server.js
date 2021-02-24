3c27a06468a63f1d27e446f7afbea4222d88cc8a
Cory <Kelly>
2021-02-24 12:03:44 -0600

    adding depoly config

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000
-// Serve all the files in '/dist' directory
-app.use(express.static('dist'));
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
+const app = express()
 
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
-});
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)

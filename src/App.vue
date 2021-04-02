5e09114a2182b80f7f49ce0d8781503c79c1f94a
Cory <Kelly>
2021-04-01 20:19:57 -0500

    - I did stuff

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <v-app>
+      <v-snackbar v-model="firstWarning" :timeout="timeout" :multi-line="multiLine">{{ firstWarningText.title }}<br/>{{ firstWarningText.body }}
+        <template v-slot:action="{ attrs }"><v-btn color="green" text v-bind="attrs" @click="firstWarning = false">
+          Close
+        </v-btn>
+        </template>
+      </v-snackbar>
+    <v-snackbar v-model="secondWarning" :timeout="timeout" :multi-line="multiLine">{{ secondWarningText.title }}<br/>{{ secondWarningText.body }}
+        <template v-slot:action="{ attrs }"><v-btn color="green" text v-bind="attrs" @click="secondWarning = false">
+          Close
+        </v-btn>
+        </template>
+      </v-snackbar>
     <v-app-bar app color="white" flat>
       <div class="d-flex align-center">
         <router-link style="display: inline-block;text-decoration: none; color: black" to="/task/all">
@@ -26,12 +38,12 @@
 
       <span style="font-weight: 500;" class="mr-1 bold pl-2">Today's 24Hr Avg</span><v-icon>mdi-thermometer</v-icon>
       <v-chip-group  v-for="soil in dailySoilTemp" :key="soil._id" active-class="accent-4 white--text">
-        <v-chip v-for="temp in soil" :key="temp._id"> <b>{{ temp.soil_temp_0to10cm.value }}°</b></v-chip>
+        <v-chip v-for="temp in soil" :key="temp._id"> <b>{{ temp.soil_temp_0to10cm.value }}{{ temp.soil_temp_0to10cm.unit }}°</b></v-chip>
       </v-chip-group>
 
       <v-icon>mdi-water</v-icon>
       <v-chip-group  v-for="soilMoist in dailySoilMoist" :key="soilMoist._id" active-class="accent-4 white--text">
-        <v-chip v-for="moist in soilMoist" :key="moist._id"> <b>{{ moist.soil_moisture_0to10cm.value }}%</b></v-chip>
+        <v-chip v-for="moist in soilMoist" :key="moist._id"> <b>{{ moist.soil_moisture_0to10cm.value }}{{  moist.soil_moisture_0to10cm.unit  }}</b></v-chip>
       </v-chip-group>
 
     </v-app-bar>
@@ -56,14 +68,27 @@ export default {
   name: 'App',
   data() {
     return {
+      filteredSoilTemp: [],
+      multiLine: true,
+      timeout: 20000,
+      firstWarning: false,
+      secondWarning: false,
+      firstWarningText: {
+        title: "Warning: FIRST round of Prodiamine.",
+        body: "This first application of Prodiamine, apply at 3 lbs/1,000 sq ft."
+      },
+      secondWarningText: {
+        title: 'Warning: SECOND round of Prodiamine',
+        body: "This first application of Prodiamine, apply at 3 lbs/1,000 sq ft."
+      },
       dailySoilTemp: null,
       dailySoilMoist: null
     }
   },
-  mounted() {
+  created() {
     this.getTools()
   },
- methods: {
+  methods: {
    async getTools() {
      let date = new Date()
      let startTime = this.getTime(date.setDate(date.getDate() - 1))
@@ -72,6 +97,7 @@ export default {
      await axios.get(`https://ag.us.clearapis.com/v1.1/daily/soil?app_id=a2f0d7a4&app_key=742a069efe55c7015c2245032fb16bbb&location=36.3986944,-86.5533952&start=${startTime}&end=${endTime}`)
          .then((response) => {
           this.dailySoilTemp = response.data
+           this.getProdiamineWarning()
          })
 
        await axios.get(`https://ag.us.clearapis.com/v1.1/daily/soil?app_id=a2f0d7a4&app_key=742a069efe55c7015c2245032fb16bbb&location=36.3986944,-86.5533952&start=${startTime}&end=${endTime}&unitcode=si-std`)
@@ -80,18 +106,26 @@ export default {
            })
    },
    getTime() {
-     let currentTime = Date.now()
-     let datePickerDate = new Date(currentTime);
-     let year = datePickerDate.getFullYear();
-     let month = datePickerDate.getMonth();
-     let day = datePickerDate.getDate();
-     let hrs = datePickerDate.getHours();
-     let mins = datePickerDate.getMinutes();
-     let sec = datePickerDate.getSeconds();
-     let miliSec = datePickerDate.getMilliseconds();
-     let time = new Date(year, month, day, hrs, mins, sec, miliSec).getTime() / 1000
-     return time;
+     return new Date().getTime() / 1000;
    },
+    getProdiamineWarning() {
+     let tempObj = []
+      for (let key in this.dailySoilTemp) {
+        let position = this.dailySoilTemp[key];
+        for (let group_key in position) {
+          let stat_group = position[group_key];
+          for (let stat_key in stat_group) {
+            tempObj.push(stat_group[stat_key])
+          }
+        }
+      }
+      this.filteredSoilTemp.push(tempObj.filter(v => v.unit === "F"))
+      if(this.filteredSoilTemp[0][0].value >= 50 && this.filteredSoilTemp[0][0].value <= 55) {
+          this.firstWarning = true
+      } else if(this.filteredSoilTemp[0][0].value >= 65 && this.filteredSoilTemp[0][0].value <= 70) {
+          this.secondWarning = true
+      }
+   }
  }
-};
+}
 </script>
